1bac1c76d384f8ff154489a0c7851c645f3c1dfd
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-06-21 09:02:57 -0500

    using match cases

--- todo.py
+++ todo.py
@@ -1,16 +1,23 @@
 """ Create a ToDo App"""
-
-user_prompt = "Enter a todo:"
 todos = []
-make_list = True
-while make_list == True:
-    todo = input(user_prompt)
-    if todo.lower() == "done":
-        print(f"my Todo list: {todos}")
-        make_list = False
-    else:
-        todos.append(todo)
-        print("Next... ")
-    print(todo)
+
+while True:
+
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.lower().strip()
+    match user_action:
+        case "add" :
+            todo = input("Enter a todo item: ")
+            todos.append(todo)
+        case "show" | "display" : #use a bitwise or OR operator
+            for item in todos:
+                item = item.title()
+                print(item)
+        case "exit":
+            break
+        case _:
+            print("Hey, the command you entered is not available!")
+
+print("Get to it!")
     
     
