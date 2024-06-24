38887755142986c0ce6db0dca954cbfec7de84b0
Felipe Contreras <felipecontreras@Felipes-Laptop.attlocal.net>
2024-06-24 16:12:00 -0500

    added other functionalities

--- todo.py
+++ todo.py
@@ -3,21 +3,27 @@ todos = []
 
 while True:
 
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.lower().strip()
     match user_action:
         case "add" :
             todo = input("Enter a todo item: ")
             todos.append(todo)
         case "show" | "display" : #use a bitwise or OR operator
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index+1}: {item.title()}"
+                print(row)
+        case "edit" :
+            number = int(input("Number of the todo item to edit: "))
+            update = input("Enter change: ")
+            todos[number-1] = update
+        case "complete":
+            number = int(input("Number of the completed todo item: "))
+            todos.pop(number-1)
         case "exit":
             break
         case _:
             print("Hey, the command you entered is not available!")
 
 print("Get to it!")
-    
     
